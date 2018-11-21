5dd3c7ef8e2d2e5aecf483bab41cb837a39b6324
Robert Van <vanyansuren@hotmail.com>
2018-11-21 23:23:17 +0400

    CocaCola And Pepsi

---
+++ AbstractFactory(Creation)/AbstractFactory(Creation)/AbstractBottle/PepsiBottle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    class PepsiBottle : AbstractBottle
+    {
+        public override void Iteract(AbstractWater water)
+        {
+            Console.WriteLine($"{this} iteract with {water}");
+        }
+    }
+}
