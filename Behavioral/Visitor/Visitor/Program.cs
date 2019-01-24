63848a90e81720a00cbc38e192cb33e53314ad56
suren-vanyan <vanyansuren@gmail.com>
2019-01-24 14:44:34 +0400

    add solutions

---
+++ Behavioral/Visitor/Visitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
